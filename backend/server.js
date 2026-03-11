70b96eda494dbf8d9d9659283d7572dfa313c0ad
HungPham097 <141068446+HungPhma@users.noreply.github.com>
2026-03-11 13:02:25 -0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,14 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
-    secure: false,
+    secure: false, // false for port 587
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_TOKEN
     },
+    tls: {
+        rejectUnauthorized: false
+    }
 });
 
 const app = express();
